789603215e8f9c1ceee224c85968449bd800799b
ziddi3 <zidthesunkid@gmail.com>
2026-08-26 11:14:51 -0600

    Configure Bircham Elk Antler e-commerce backend

---
+++ store-config.js
@@ -0,0 +1,11 @@
+export const STORE_CONFIG = {
+  storeName: "Bircham Elk Antler",
+  mode: "ecommerce",
+  leadScraperEnabled: false,
+  pplMatchingEnabled: false,
+  paymentGateway: "stripe_standard",
+  shippingZones: ["Canada", "United States", "International"],
+  inventoryTracking: true,
+  taxBehavior: "exclusive_plus_gst",
+  supportContact: "orders@birchamelkantler.com"
+};
